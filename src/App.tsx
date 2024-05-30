9551894383522111fa46a557f8bc3668945b19a6
jesuan <jespaterm@gmail.com>
2024-05-30 19:50:53 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      <div className='container'>
+        <h1>Portfolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Navbar: React.FC = () => {
+
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    )
+}
+
+export default Navbar

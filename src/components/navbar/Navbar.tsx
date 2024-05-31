d0800a373227e843014ce668f96706e15905a022
jesuan <jespaterm@gmail.com>
2024-05-30 20:39:50 -0400

    Agregando el NavBar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,26 @@
 import React from "react"
+import { NAVIGATION } from "../../const"
+import s from './Navbar.module.css'
 
 const Navbar: React.FC = () => {
 
+
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className={s.container}>
+            <ul className={s.ul}>
+                {
+                    Object.entries(NAVIGATION).map(([key, {literal, href}]) =>
+                        <li key={key} className={s.li}>
+                            <a
+                                href={href}
+                                className={s.link}
+                            >
+                                {literal}
+                            </a>
+                        </li>
+                    )
+                }
+            </ul>
         </div>
     )
 }

e600acd62964d36b3109093583f5cae0dd09d238
jesuan <jespaterm@gmail.com>
2024-05-30 20:40:33 -0400

    Agregando el archivo const para crear constantes

---
+++ src/const.ts
@@ -0,0 +1,18 @@
+export const NAVIGATION = {
+    sobremi: {
+        literal: 'Sobre mí',
+        href: '#sobre-mi'
+    },
+    proyectos: {
+        literal: 'Proyectos',
+        href: '#proyectos'
+    },
+    social: {
+        literal: 'Social',
+        href: '#social'
+    },
+    contacto: {
+        literal: 'Contacto',
+        href: '#contacto'
+    }
+}
